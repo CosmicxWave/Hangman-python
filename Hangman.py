733593b62027fd955e6571b025bde425614f5a62
Cosmic <prateekpattanaik2007@gmail.com>
2026-01-19 00:26:07 +0530

    Increased number of words

--- Hangman.py
+++ Hangman.py
@@ -27,7 +27,25 @@ def display_man(wrong_attempts):
         print(i)
 
 
-word_bank = ['Rizz','Keys','Rocket','Thrust','Flight']
+word_bank = ['Rizz','Keys','Rocket','Thrust','Flight','electricity', 'donkey',
+ 'engineering', 'hangman', 'circuit', 'imagination', 'robot', 'memory', 'power', 
+ 'submarine', 'chess', 'resistance', 'matrix', 'function', 'laser', 'mechanism', 
+ 'bodyguard', 'titanic', 'global', 'ozone', 'bridge', 'technology', 'spider',
+ 'pyramid', 'sphere', 'member', 'warning', 'yourself', 'screen', 'language',
+ 'system', 'internet', 'parameter', 'traffic', 'network', 'filter', 'nucleus',
+ 'automatic', 'microphone', 'cassette', 'operation', 'country', 'beautiful',
+ 'picture', 'teacher', 'superman', 'undertaker', 'alarm', 'process', 'keyboard', 
+ 'electron', 'certificate', 'grandfather', 'landmark', 'relativity', 'eraser',
+ 'design', 'football', 'human', 'musician', 'egyptian', 'elephant', 'queen',
+ 'message', 'wallpaper', 'nationality', 'answer', 'wrong', 'statement', 'forest',
+ 'puzzle', 'voltage', 'current', 'mathematics', 'wisdom', 'dream', 'supermarket',
+ 'database', 'collection', 'barrier', 'project', 'sunlight', 'figure', 'graph',
+ 'battle', 'hundred', 'signal', 'thousand', 'transformation', 'daughter', 'flower',
+ 'communication', 'microwave', 'electronic', 'peace', 'wireless', 'delete', 'wind',
+ 'brain', 'control', 'prophet', 'freedom', 'harbour', 'confidence', 'positive', 'absolute',
+ 'harvest', 'hunger', 'woman', 'children', 'stranger', 'garden', 'pleasure', 'cinema',
+ 'hardware', 'xerox', 'transistor', 'computer']
+
 word = random.choice(word_bank).lower()
 guessed_word = len(word) * ['_']
 wrong_attempts = 0
